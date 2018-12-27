0e84c259b66a1fd87fa1a617efbee2eb8354658e
meixiaoxi <meixiaoxi110@gmail.com>
2018-12-27 10:42:37 +0800

    build.sh修改打包方式为tar包

--- build.sh
+++ build.sh
@@ -30,5 +30,5 @@ cp -rf $Startup_Dir/target/lts-bin/lts/lib  $Dist_Bin_Dir/war/jetty
 cp -rf $LTS_Bin_Dir/lts-admin/target/lts-admin-$VERSION.war $Dist_Bin_Dir/war/lts-admin.war
 
  cd $LTS_Bin_Dir/dist
- zip -r lts-$VERSION-bin.zip lts-$VERSION-bin/*
+ tar -czvf lts-$VERSION-bin.tar.gz lts-$VERSION-bin/*
  rm -rf lts-$VERSION-bin
